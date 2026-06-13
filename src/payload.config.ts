e622dc799b5fe7538f88db0bdd824a2a25c5d82b
Alfredo Dumaine <dumainealfredo@gmail.com>
2026-06-13 00:26:40 -0600

    chore(admin): restore default Payload admin logo and icon

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -124,12 +124,6 @@ export default buildConfig({
     importMap: {
       baseDir: path.resolve(dirname),
     },
-    components: {
-      graphics: {
-        Logo: '/components/admin/AdminLogo',
-        Icon: '/components/admin/AdminIcon',
-      },
-    },
   },
   collections: [Users, Media, MediaVideo, Tours, Bookings],
   globals: [
